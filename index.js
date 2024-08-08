4edcc1a2095b92d10c6cd4c99c423c766a04d352
Giovana Cassias <giovanacassias@outlook.com>
2024-08-08 19:57:09 -0300

    stats ok

--- index.js
+++ index.js
@@ -55,6 +55,27 @@
                 document.getElementById("type-container-2").style.display = "none";
             };
 
+            //ATTACK
+            const pokemonAttack = data.stats[1].base_stat;
+            const scoreAttack = document.getElementById("attack-score");
+            scoreAttack.innerHTML = pokemonAttack;
+
+            //DEFENSE
+            const pokemonDefense = data.stats[2].base_stat;
+            const scoreDefense = document.getElementById("defense-score");
+            scoreDefense.innerHTML = pokemonDefense;
+
+            //SP. ATTACK
+            const pokemonSpecialAttack = data.stats[3].base_stat;
+            const scoreSpAttack = document.getElementById("special-attack-score");
+            scoreSpAttack.innerHTML = pokemonSpecialAttack;
+
+            //SP. DEFENSE
+            const pokemonSpecialDefense = data.stats[4].base_stat;
+            const scoreSpDefense = document.getElementById("special-defense-score");
+            scoreSpDefense.innerHTML = pokemonSpecialDefense;
+
+            //CLEANING THE SEARCH BAR
             document.getElementById('input-pokemon-name').value = '';
              
         }
