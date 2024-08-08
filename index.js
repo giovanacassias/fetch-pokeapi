9ab13780c0b569990babbf41606d1b57e05f6cea
Giovana Cassias <giovanacassias@outlook.com>
2024-08-08 19:23:32 -0300

    main completo

--- index.js
+++ index.js
@@ -14,19 +14,49 @@
 
         try{
 
-            const pokemonName = document.getElementById('input-pokemon-name').value.toLowerCase();
+            const pokemonInput = document.getElementById('input-pokemon-name').value.toLowerCase();
 
-            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInput}`);
 
             if(!response.ok){
                 throw new Error("Could not fetch resource!");
             }
             
             const data = await response.json();
+            
+            
+            //SPRITE
             const pokemonSprite = data.sprites.front_default;
             const imgElement = document.getElementById("pokemonSprite");
             imgElement.src = pokemonSprite;
             imgElement.style.display = "block";
+
+            //NAME
+            const pokemonName = data.name;
+            const h1 = document.getElementById("pokemon-name");
+            h1.innerHTML = pokemonName;
+
+            //ID
+            const pokemonID = data.id;
+            const h2 = document.getElementById("index-num");
+            h2.innerHTML = `#${pokemonID}`;
+
+            //TYPE 1
+            const pokemonType1 = data.types[0].type.name;
+            const type1 = document.getElementById("type-1");
+            type1.innerHTML = pokemonType1;
+
+            if(data.types.length > 1){
+                const pokemonType2 = data.types[1].type.name;
+                const type2 = document.getElementById("type-2");
+                type2.innerHTML = pokemonType2;
+                document.getElementById("type-container-2").style.display = "flex";
+            }else{
+                document.getElementById("type-container-2").style.display = "none";
+            };
+
+            document.getElementById('input-pokemon-name').value = '';
+             
         }
         catch(error){
             console.log(error);
