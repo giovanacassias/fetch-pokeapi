1b4e14f6923efed6f9e5e6fd3ed4042cb89caa17
Giovana Cassias <giovanacassias@outlook.com>
2024-08-08 16:54:49 -0300

    fetch img ok

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
 
         try{
 
-            const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+            const pokemonName = document.getElementById('input-pokemon-name').value.toLowerCase();
 
             const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
